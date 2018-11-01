5974dfe4d1164bf47190b965ae812e9070d240b2
Eamon Walshe <eamon@walshe.org>
2018-11-01 22:27:12 +0000

    Update docker host

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ cd /var/www/scrumonline
 /usr/bin/php ./bin/composer install
 cp src/sample-config.php src/config.php
 # Overwrite host
-echo '$host = "http://localhost:8080";' >> src/config.php
+#echo '$host = "http://localhost:8080";' >> src/config.php
+echo '$host = "http://planningpoker.walshe.org";' >> src/config.php
